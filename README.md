443f8e9d129da082c5dd9b9fb09dbbeb9e1d98dc
gfreycode <gfreycode@gmail.com>
2021-07-02 07:06:16 -0500

    y a termine el la estructura y la logica del taller 2

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # math-aplication-JS-PD
+
+# Porcentajes y Descuentos
+
+- Primer paso: definir las formulas
+- Segundo paso: implementar las formulas en JS
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
---
+++ src/index.js
@@ -0,0 +1,53 @@
+
+
+const calculateDisconuntInThePrice = function (valuePrice, valueDiscount) {
+    const percentageDiscountedPrice = 100 - valueDiscount;
+    const priceWithDiscount = (valuePrice * percentageDiscountedPrice) / 100;
+
+    return priceWithDiscount
+};
+
+const valueOfDiscount = function (valuePrice, valueDiscount) {
+    const percentageDiscountedPrice = 100 - valueDiscount;
+    const priceWithDiscount = (valuePrice * percentageDiscountedPrice) / 100;
+
+    let valorDiscount = valuePrice - priceWithDiscount;
+    return valorDiscount
+};
+
+const verificationCupon = function () {
+    const cupons = [10, 25, 50];
+    const cupon = document.getElementById("cupons");
+    const valueCupon = parseInt(cupon.value);
+    if (valueCupon === cupons[0]) {
+        return cupons[0];
+    } else if (valueCupon === cupons[1]) {
+        return cupons[1];
+    } else if (valueCupon === cupons[2]) {
+        return cupons[2];
+    } else {
+        return 0
+    }
+}
+
+const onClickButtonPriceDiscount = function () {
+    const originalPrice = document.getElementById("inputPrice");
+    const discount = document.getElementById("inputDiscount");
+    const valuePrice = parseInt(originalPrice.value);
+    const valueDiscount = parseInt(discount.value);
+    const verificationCupons = verificationCupon();
+    const discountCupon = valueDiscount + verificationCupons;
+
+    const priceWithDiscount = calculateDisconuntInThePrice(valuePrice, discountCupon);
+    const valorDiscount = valueOfDiscount(valuePrice, discountCupon);
+
+    const displayPrice = document.getElementById("displayPrice");
+    const price = valuePrice;
+    displayPrice.innerText = `$${price}`;
+
+    const displayDiscount = document.getElementById("displayDiscount");
+    displayDiscount.innerText = `$${priceWithDiscount}`;
+
+    const displayPercentageDiscount = document.getElementById("displayPercentageDiscount");
+    displayPercentageDiscount.innerText = `-${discountCupon}%`
+}
